042adab6a88fb30317b08a62bafdc64593a0bae5
gileswendes-hue <gileswendes@gmail.com>
2026-01-13 00:07:04 +0000

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     API_BASE_URL: '/api/words',
     SCORE_API_URL: '/api/scores',
-    APP_VERSION: '6.4.5',
+    APP_VERSION: '6.4.6',
     KIDS_LIST_FILE: 'kids_words.txt',
     SPECIAL: {
         CAKE: { text: 'CAKE', prob: 0.005, fade: 300, msg: "The cake is a lie!", dur: 3000 },
