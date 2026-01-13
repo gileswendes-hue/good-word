6d8159dd48f8daa21ab897bf53d0b35be7ccc4b3
gileswendes-hue <gileswendes@gmail.com>
2026-01-13 00:06:16 +0000

    Update script.js

--- public/script.js
+++ public/script.js
@@ -16,7 +16,7 @@ const CONFIG = {
     TIP_COOLDOWN: 4,
     HISTORY_SIZE: 500,
     VOTE: {
-        MASH_LIMIT: 8,           
+        MASH_LIMIT: 12,           
         COOLDOWN_TIERS: [5, 10, 20],  
         STREAK_WINDOW: 1400,      
         SWIPE_THRESHOLD: 100
